988a8e9c12c91355028ae13854eabb33c750966d
Cassio M. M. Pereira <cassiomartini@gmail.com>
2013-07-23 21:13:37 -0300

    working on double hist

--- src/TSanalysis.jl
+++ src/TSanalysis.jl
@@ -2,13 +2,40 @@ module TSanalysis
 
 export ami_calc
 
-function ami_calc(dataset::Array{Float64,2}, ncoef::Int64)
-    return true
+function range(x::Vector{Float64})
+    return [min(x), max(x)]
 end
 
-function hello()
-    return false
+function double_hist(series::Vector{Float64}, lag::Int64, partitions::Int64)
+    # return a double histogram of the probabilities of observations,
+    # considering the specified lag.
 end
 
+function ami_calc(dataset::Matrix{Float64}; partitions::Int64 = 16, lagmax::Int64 = 20)
+    # we will operate on columns:
+    D = dataset'
+    n = size(D)[2]
+    A = zeros(lagmax,n)
+    for i in 1:n
+        series = D[:,i]
+        series = (series - min(series)) / diff(range(series))
+        corr = zeros(lagmax)
+        for j in 1:lagmax
+            hist = zeros(partitions, partitions)
+
+        end
+        break
+    end
+    #@show A
+end
+
+srand(1234)
+
+X = rand(10,100)
+T = rand(10, 100)
+
+A = ami_calc(X)
+
+#writedlm("ami.dat", X) 
 
 end
